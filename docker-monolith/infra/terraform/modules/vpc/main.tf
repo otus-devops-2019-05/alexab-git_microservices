2de0d50d0809b51dcab8283c38f963ac0276802d
alex <xyzuser@yandex.ru>
2019-08-15 17:00:05 +0300

    infra - docker app playbook added

--- docker-monolith/infra/terraform/modules/vpc/main.tf
+++ docker-monolith/infra/terraform/modules/vpc/main.tf
@@ -1,11 +1,14 @@
-resource "google_compute_firewall" "reddit_firewall_ssh" {
-  name    = "default-allow-reddit-ssh"
+
+resource "google_compute_firewall" "reddit_app_port" {
+  name    = "allow-reddit-app-port"
   network = "default"
 
   allow {
     protocol = "tcp"
-    ports    = ["22"]
+    ports    = ["9292"]
   }
 
+  target_tags   = ["reddit-app"]
   source_ranges = "${var.source_ranges}"
 }
+
