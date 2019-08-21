abf7677349df141ec43a7ddb6f528a69f10ced6a
alex <xyzuser@yandex.ru>
2019-08-21 20:21:14 +0300

    HW#13 microservices - docker containers OS moved from ubuntu to apline

--- tools/drun.sh
+++ tools/drun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-NET_NAME="reddit-stage"
+NET_NAME="reddit"
 docker network create $NET_NAME
 docker run -d --network=$NET_NAME --network-alias=post_db --network-alias=comment_db mongo:latest
 docker run -d --network=$NET_NAME --network-alias=post xyzuser/post:1.0
 docker run -d --network=$NET_NAME --network-alias=comment xyzuser/comment:1.0
-docker run -d --network=$NET_NAME -p 9292:9292 xyzuser/ui:1.0
+docker run -d --network=$NET_NAME -p 9292:9292 xyzuser/ui:2.0
