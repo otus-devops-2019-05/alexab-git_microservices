f8c9a03e73a953fcbaa4e97d9834d0e13e7212a7
alex <xyzuser@yandex.ru>
2019-08-21 19:19:59 +0300

    HW#13 microservices part3

--- tools/drun.sh
+++ tools/drun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
-docker network create reddit
-docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db mongo:latest
-docker run -d --network=reddit --network-alias=post xyzuser/post:1.0
-docker run -d --network=reddit --network-alias=comment xyzuser/comment:1.0
-docker run -d --network=reddit -p 9292:9292 xyzuser/ui:1.0
+NET_NAME="reddit-stage"
+docker network create $NET_NAME
+docker run -d --network=$NET_NAME --network-alias=post_db --network-alias=comment_db mongo:latest
+docker run -d --network=$NET_NAME --network-alias=post xyzuser/post:1.0
+docker run -d --network=$NET_NAME --network-alias=comment xyzuser/comment:1.0
+docker run -d --network=$NET_NAME -p 9292:9292 xyzuser/ui:1.0
